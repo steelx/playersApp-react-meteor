981f40c7575e777d4d761dee54bbe337f4af0410
Ajinkya Borade <ajinkyaborade@Ajinkyas-MacBook-Pro.local>
2017-12-13 17:31:14 +0530

    added react simple component

--- client/main.js
+++ client/main.js
@@ -1,1 +1,19 @@
-console.log('main.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(function _startup(){
+    let title = 'Players App Meteor+React';
+    let name = 'Ajinkya';
+
+    let Html = () => {
+        return (
+            <div>
+                <h2>{title}</h2>
+                <p>Hello from {name}.</p>
+            </div>
+        );
+    }
+
+    ReactDOM.render(<Html />, document.getElementById('app'));
+});
